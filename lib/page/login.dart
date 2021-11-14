eb18c09c5b27400f1adffbbd632abb633604fbad
deartis <deartissolu@gmail.com>
2021-11-14 19:25:36 -0300

    Tela de login

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../theme/cores.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -8,15 +9,75 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  _entrar() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              Text('Login'),
-            ],
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'ADM',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                const Text('painel',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Cores.cor4,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "E-mail",
+                        hintText: "E-mail"),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Senha",
+                        hintText: 'Senha'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: ElevatedButton(
+                    onPressed: _entrar,
+                    child: const Text('Entrar',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    style: ElevatedButton.styleFrom(
+                      primary: Cores.cor7,
+                      padding: const EdgeInsets.all(10),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: TextButton(
+                      onPressed: () {}, child: const Text('Esqueceu a senha?')),
+                )
+              ],
+            ),
           ),
         ),
       ),
