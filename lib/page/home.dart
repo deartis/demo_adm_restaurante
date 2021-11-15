b5f69bbd5a221edb50e68989c4330d95e60146be
deartis <deartissolu@gmail.com>
2021-11-14 21:47:12 -0300

    Criação Drawer

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,3 +1,4 @@
+import 'package:demo_adm_restaurante/theme/cores.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,12 +12,53 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              child: Text("Navegação"),
+              decoration: BoxDecoration(
+                color: Cores.cor1,
+              ),
+            ),
+            ListTile(
+              title: const Text("Pratos"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Pedidos"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Opções"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Balanço"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Administrativo"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text("Home"),
+        titleSpacing: 100,
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              Text('Pagina'),
-            ],
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: const [
+                Text('Pagina'),
+              ],
+            ),
           ),
         ),
       ),
