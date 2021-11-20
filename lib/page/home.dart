4d807d373ba963b217063e01e6a8185ec16f7a07
deartis <deartissolu@gmail.com>
2021-11-20 16:05:04 -0300

    telas home

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -12,23 +12,101 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+//Widget do texto ativado/desativado
+class TxtEstadoaTUAL {
+  _getTxt(_estadoCardapio, _txtEstadoCardapio) {
+    if (_estadoCardapio) {
+      return Text(
+        _txtEstadoCardapio,
+        style: const TextStyle(color: Cores.cor6),
+      );
+    } else {
+      return Text(
+        _txtEstadoCardapio,
+        style: const TextStyle(color: Cores.cor7),
+      );
+    }
+  }
+}
+
 class _HomeState extends State<Home> {
+  static bool _estadoCardapio = false;
+  static String _txtEstadoCardapio = 'Desativado';
+
   _telas(String tela) {
     switch (tela) {
       case 'tela2':
         return Card(
           child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: const Border(
+                  top: BorderSide(width: 1, color: Cores.cor4),
+                  left: BorderSide(width: 1, color: Cores.cor4),
+                  right: BorderSide(width: 1, color: Cores.cor4),
+                  bottom: BorderSide(width: 1, color: Cores.cor4),
+                )),
             padding: const EdgeInsets.all(10),
             child: Column(
               children: [
                 Container(
                     decoration: const BoxDecoration(border: Border.symmetric()),
                     padding: const EdgeInsets.all(5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text('Cardápio'),
-                        Text('Desativado'),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text(
+                              'Cardápio',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const Divider(
+                          color: Cores.cor4,
+                          height: 1,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TxtEstadoaTUAL()
+                                ._getTxt(_estadoCardapio, _txtEstadoCardapio),
+                            Switch(
+                              value: _estadoCardapio,
+                              onChanged: (v) => {
+                                setState(() {
+                                  _estadoCardapio = v;
+                                  if (_estadoCardapio) {
+                                    _txtEstadoCardapio = 'Ativado';
+                                  } else {
+                                    _txtEstadoCardapio = 'Desativado';
+                                  }
+                                })
+                              },
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: const Border(
+                                top: BorderSide(width: 1, color: Cores.cor4),
+                                left: BorderSide(width: 1, color: Cores.cor4),
+                                right: BorderSide(width: 1, color: Cores.cor4),
+                                bottom: BorderSide(width: 1, color: Cores.cor4),
+                              )),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: const [
+                              Text('Frango a parmegiana'),
+                              Text('Inhoque'),
+                              Text('Bife com Frita'),
+                            ],
+                          ),
+                        ),
                       ],
                     ))
               ],
